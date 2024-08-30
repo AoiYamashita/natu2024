5033e82940d439956ccc6762355f984525801b16
yamashita <yamashitaaoi1230@icloud.com>
2024-08-30 22:23:31 +0900

    save

--- install/autoRobo/lib/python3.10/site-packages/autoRobo/operater.py
+++ install/autoRobo/lib/python3.10/site-packages/autoRobo/operater.py
@@ -11,13 +11,15 @@ class Operate(Node):
         super().__init__('operater')
         self.sub = self.create_subscription(Vector3,"Pose",self.cb,10)
         self.subP = self.create_subscription(Vector3,"destination",self.setdes,10)
-        self.destination = np.array([1550,900,np.pi])
+        self.destination = None#np.array([1800-200-45,900,np.pi])
         self.pub = self.create_publisher(String,"serial_data",10)
         self.rate = 0.001
         self.prate = 20000
-        self.wrate = 2000
+        self.wrate = 1000
         return
     def cb(self,data):
+        if self.destination is None:
+            return
         x = data.x
         y = data.y
         theta = data.z
@@ -30,13 +32,17 @@ class Operate(Node):
         t = 0.01
         next = (A0*(1 - t)**3 + 3*A1*t*(1 - t)**2 + 3*B1*t**2*(1 - t) + B0*t**3)
         delta = next - A0
-        omega = self.wrate*(self.destination[2] - theta)
+        omega = self.wrate*np.sin(self.destination[2] - theta)
+        
         dig = (np.arctan2(delta[1],delta[0])-theta)
         speed = self.prate*np.sqrt(delta[0]**2+delta[1]**2)
         s = String()
-        s.data = str("dig:{:.3f},speed:{},Rspeed:{}".format(float(dig),int(speed),int(omega)))
+        s.data = str("move:{:.3f},{},{}".format(float(dig),int(speed),int(omega)))
+        #s.data = str("dig:{:.3f},speed:{},Rspeed:{}".format(float(0),int(0),int(3000)))
+
         self.pub.publish(s)
-        self.get_logger().info("send: %s" % "dig:{:.3f},speed:{},Rspeed:{}".format(float(dig),int(speed),int(omega)))
+        self.get_logger().info("send: %s" % "move:{:.3f},{},{}".format(float(dig),int(speed),int(omega)))
+        
         return
     def setdes(self,data):
         self.destination = np.array([data.x,data.y,data.z])
