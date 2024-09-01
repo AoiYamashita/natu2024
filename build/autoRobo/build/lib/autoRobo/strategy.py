b662c091d2c9b900313623530bd96562811b5bb3
yamashita <yamashitaaoi1230@icloud.com>
2024-09-01 11:51:06 +0900

    comp

--- build/autoRobo/build/lib/autoRobo/strategy.py
+++ build/autoRobo/build/lib/autoRobo/strategy.py
@@ -11,9 +11,26 @@ class Strtegy(Node):
         self.slidepub = self.create_publisher(String,"serial_data",10)
         self.destpub = self.create_publisher(Vector3,"destination",10)
         self.posesub = self.create_subscription(Vector3,"Pose",self.cb,10)
-        self.poses = [np.array([1250,1250,np.pi*5/4]),"slide",np.array([1500,1500,np.pi*5/4]),"slide",np.array([1300,600,7/4*np.pi]),np.array([1500,300,7/4*np.pi]),"slide",np.array([1200,900,0.0]),np.array([1400,900,np.pi/2.0]),np.array([1400,900,np.pi]),np.array([1200,900,np.pi]),"slide"]
+        self.poses = [np.array([1250,1250,np.pi*5/4]),
+                      "slide",
+                      np.array([1500,1500,np.pi*5/4]),
+                      "slide",
+                      np.array([1300,600,7/4*np.pi]),
+                      np.array([1500,300,7/4*np.pi]),
+                      "slide",
+                      np.array([1200,900,0.0]),
+                      np.array([1500,900,np.pi/2.0]),
+                      np.array([1500,900,np.pi]),
+                      np.array([1200,900,np.pi]),
+                      "slide",
+                      np.array([1550,900,np.pi]),
+                      "finish"]
         self.fase = 0
     def cb(self,data):
+        if self.poses[self.fase] == "finish":
+            self.get_logger().info("finish task")
+            return
+        self.get_logger().info("now: %d" % self.fase)
         pose = np.array([data.x,data.y,data.z])
         if self.poses[self.fase] == "slide":
             s = String()
@@ -23,7 +40,7 @@ class Strtegy(Node):
             self.fase += 1
             return
         delta = self.poses[self.fase] - pose
-        if abs(delta[0]) < 30 and abs(delta[1]) < 30 and abs(delta[2]) < np.pi/12:
+        if abs(delta[0]) < 30 and abs(delta[1]) < 30 and np.tan(delta[2]) < np.tan(np.pi/12):
             self.fase += 1
         else:
             V = Vector3()
